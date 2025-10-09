865072f5e815bd260324148f88328b05d7eeb6e5
vlad <vladsokolovskij67@gmail.com>
2025-10-09 22:45:43 +0300

    work base pr

--- e2e-framework/docker/entrypoint.sh
+++ e2e-framework/docker/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 echo "=== Starting E2E tests ==="
-pytest -v --alluredir=reports/allure
+
+REPORTS_DIR=${REPORTS_DIR:-reports/allure-results}
+
+mkdir -p "${REPORTS_DIR}"
+
+pytest -v --alluredir="${REPORTS_DIR}"
+exit $?
